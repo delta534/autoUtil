efd92157bd443fce654aa5db351e4ac7528a7462
delta534 <mlane534@gmail.com>
2014-03-08 10:49:03 -0800

    Moving something and fixing some deprecation issues

--- src/info/jbcs/minecraft/autoutils/Autoutils.java
+++ src/info/jbcs/minecraft/autoutils/Autoutils.java
@@ -1,15 +1,13 @@
 package info.jbcs.minecraft.autoutils;
 
+import cpw.mods.fml.common.Mod;
 import info.jbcs.minecraft.utilities.packets.PacketHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraftforge.common.Configuration;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.Mod.PostInit;
-import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -29,22 +27,22 @@ public class Autoutils implements IPacketHandler {
 	@SidedProxy(clientSide = "info.jbcs.minecraft.autoutils.ProxyClient", serverSide = "info.jbcs.minecraft.autoutils.Proxy")
 	public static Proxy proxy;
 
-	@PreInit
+    @EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		config = new Configuration(event.getSuggestedConfigurationFile());
 		config.load();
 		
 		proxy.preInit();
 	}
-	
-	@Init
+
+    @EventHandler
 	public void init(FMLInitializationEvent event) {
 		proxy.init();
 
 		PacketHandler.register(this);
 	}
 
-	@PostInit
+    @EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
 	}
 
